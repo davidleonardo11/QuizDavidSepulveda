ddf273a21f7b60f47a7cdbf541f3f9de8dd909e4
davidleonardo11 <62727860+davidleonardo11@users.noreply.github.com>
2020-08-29 13:13:48 -0500

    Add files via upload

---
+++ Quiz_DavidSepulveda/Home.xaml.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Quiz_DavidSepulveda
+{
+    /// <summary>
+    /// Lógica de interacción para Home.xaml
+    /// </summary>
+    public partial class Home : Page
+    {
+        public Home()
+        {
+            InitializeComponent();
+            cmbCategoria.Items.Add("Pollo queso");
+            cmbCategoria.Items.Add("Carnes frias");
+            cmbCategoria.Items.Add("Llanera");
+            cmbCategoria.Items.Add("The Chavez");
+            cmbCategoria.Items.Add("Hawaiana");
+            cmbVegana.Items.Add("Mamma Tomato");
+            cmbVegana.Items.Add("Morelia");
+            cmbVegana.Items.Add("Veggie Pizza");
+            cmbVegana.Items.Add("The Chavez");
+            cmbVegana.Items.Add("Green's Pizza");
+
+        }
+
+        
+
+        private void cbxCarnes_Checked(object sender, RoutedEventArgs e)
+        {
+            if(cbxCarnes.IsChecked==true)
+            {
+                cmbCategoria.Visibility = Visibility.Visible;
+            }
+            
+
+        }
+
+       
+
+        private void cbxVegana_Checked(object sender, RoutedEventArgs e)
+        {
+            if(cbxVegana.IsChecked==true)
+            {
+                cmbVegana.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void cbxVegana_Unchecked(object sender, RoutedEventArgs e)
+        {
+            cmbVegana.Visibility = Visibility.Hidden;
+        }
+
+        private void cbxCarnes_Unchecked(object sender, RoutedEventArgs e)
+        {
+            cmbCategoria.Visibility = Visibility.Hidden;
+        }
+
+        
+
+        private void btnSeleccionar_Click(object sender, RoutedEventArgs e)
+        {
+               if(cbxCarnes.IsChecked==true)
+            {
+                txtCarrito.Items.Add(cmbCategoria.SelectedItem);
+            }
+            else
+            {
+                txtCarrito.Items.Add(cmbVegana.SelectedItem);
+            }   
+
+                
+            
+            
+           
+        }
+        
+
+        private void btnAgendar_Click(object sender, RoutedEventArgs e)
+        {
+            lblFecha.Content = calAlmanaque.SelectedDate.ToString();
+            lblFecha.Visibility = Visibility.Visible;
+        }
+
+        private void btnPedido_Click(object sender, RoutedEventArgs e)
+        {
+
+            MessageBox.Show("Tu pedido ha sido exitoso. Gracias por tu pedido");
+        }
+
+        private void btnCerrarSesion_Click(object sender, RoutedEventArgs e)
+        {
+            
+            MainWindow navegar = (MainWindow)Window.GetWindow(this);
+            navegar.frameLogin.NavigationService.Navigate(new InicioSesion());
+        }
+
+        private void btnQuitar_Click(object sender, RoutedEventArgs e)
+        {
+            if (cbxCarnes.IsChecked == true)
+            {
+                txtCarrito.Items.Remove(cmbCategoria.SelectedItem);
+            }
+            else
+            {
+                txtCarrito.Items.Remove(cmbVegana.SelectedItem);
+            }
+        } /*estos es una prueba*/ 
+
+        
+    }
+}
---
+++ Quiz_DavidSepulveda/InicioSesion.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Quiz_DavidSepulveda
+{
+    /// <summary>
+    /// Lógica de interacción para InicioSesion.xaml
+    /// </summary>
+    public partial class InicioSesion : Page
+    {
+        public InicioSesion()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIniciar_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtUsuario.Text == "David" && txtPassword.Password == "1234")
+            {
+                
+                MainWindow navegar = (MainWindow)Window.GetWindow(this);
+                navegar.frameLogin.NavigationService.Navigate(new Home());
+                
+               
+            }
+            else
+            {
+                txtUsuario.Text = "";
+                txtPassword.Password = "";
+                MessageBox.Show("Usuario o Clave incorrecta");
+            }
+        }
+    }
+}
